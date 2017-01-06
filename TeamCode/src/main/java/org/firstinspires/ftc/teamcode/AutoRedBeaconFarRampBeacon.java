86085b77a7d753df8284d595044287693666f158
jeremyyao <jeremysemailusa@gmail.com>
2017-01-05 20:27:34 -0500

    changed methodology of tracking scans; applied this to red version

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedBeaconFarRampBeacon.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedBeaconFarRampBeacon.java
@@ -13,8 +13,7 @@ public class AutoRedBeaconFarRampBeacon extends LinearOpMode
     private OmniDriveBot robot = new OmniDriveBot();
     public void runOpMode() throws InterruptedException
     {
-        boolean isRedOnLeft = true;
-//      short timesScanned = 0; MAy be needed to make it so thath robot doesn't shimmy/scan until it hits wall
+        int timesScanned = 0;
         robot.init(hardwareMap);
 
         waitForStart();
@@ -37,15 +36,14 @@ public class AutoRedBeaconFarRampBeacon extends LinearOpMode
         sleep(100);
         while(!robot.isDetectingRed())
         {
-            if (robot.isDetectingBlue())
-                isRedOnLeft = false;
+            if (timesScanned > 17)
+                break;
+
+            timesScanned++;
             robot.driveStraight(1,0,.87);
             sleep(100);
         }
         sleep(100);
-
-        if (isRedOnLeft)
-            robot.driveStraight(3,0,.87);
 
         while(!gamepad1.a){}
         robot.driveStraight(3,90,.87);
@@ -53,12 +51,7 @@ public class AutoRedBeaconFarRampBeacon extends LinearOpMode
         robot.driveStraight(1,-90,.87);
 
         while(!gamepad1.a){}
-        //robot.driveStraight(40,180,.87);
-        if (isRedOnLeft)
-            robot.driveStraight(27, 0, .87);
-        else if (!isRedOnLeft)
-            robot.driveStraight(22, 0, .87);
-
+        robot.driveStraight(32 - timesScanned, 0, .87);
         robot.driveStraight(9, 90,.38);
 
         while(!gamepad1.a){}
@@ -66,34 +59,33 @@ public class AutoRedBeaconFarRampBeacon extends LinearOpMode
         robot.driveStraight(19, 0, .87);
 
         while(!gamepad1.a){}
-        isRedOnLeft = true;
+        timesScanned = 0;
         while(!robot.isDetectingRed())
         {
-            if (robot.isDetectingBlue())
-                isRedOnLeft = false;
+            if (timesScanned > 17)
+                break;
+
             robot.driveStraight(1,0,.87);
             sleep(200);
         }
-
-        if (isRedOnLeft)
-            robot.driveStraight(3,0,.87);
 
         while(!gamepad1.a){}
         robot.driveStraight(6,90,.87);
         sleep(100);
 
         while(!gamepad1.a){}
-        if (!isRedOnLeft)
-        {
-            robot.driveStraight(64, -143, .95);
-            robot.spin(180);
-            robot.driveStraight(8, 35, .95);
-        }
-        else if (isRedOnLeft)
+        if (timesScanned <= 4)
         {
             robot.driveStraight(70,-138,.95);
             robot.spin(180);
             robot.driveStraight(8, 42, .95);
         }
+        else
+        {
+            robot.driveStraight(64, -143, .95);
+            robot.spin(180);
+            robot.driveStraight(8, 35, .95);
+        }
+
     }
 }
