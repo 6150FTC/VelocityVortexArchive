73b23fe79f13811d65a9127b1a4d2877a1a5df04
jeremyyao <jeremysemailusa@gmail.com>
2017-01-10 20:14:09 -0500

    Update auto code; add comments in servotest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedBeaconFarRampBeacon.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedBeaconFarRampBeacon.java
@@ -19,73 +19,57 @@ public class AutoRedBeaconFarRampBeacon extends LinearOpMode
         waitForStart();
 
         //A suggestion but add part to shoot particles into center washing machine
-        while(!gamepad1.a){}
         /*load balls, needs testing
 
          */
         robot.driveStraight(90, 153,.87);
         sleep(100);
-        while(!gamepad1.a){}
+
         robot.spin(90);
         sleep(100);
-        while(!gamepad1.a){}
-        robot.driveStraight(29,90,.87);
 
-        while(!gamepad1.a){}
+        robot.driveStraight(30,90,.87);
+
         //Next beacon,
         sleep(100);
-        while(!robot.isDetectingRed())
-        {
-            if (timesScanned > 17)
-                break;
-
-            timesScanned++;
-            robot.driveStraight(1,0,.87);
-            sleep(100);
-        }
-        sleep(100);
-
-        while(!gamepad1.a){}
-        robot.driveStraight(3,90,.87);
-        sleep(100);
-        robot.driveStraight(1,-90,.87);
-
-        while(!gamepad1.a){}
-        robot.driveStraight(32 - timesScanned, 0, .87);
-        robot.driveStraight(9, 90,.38);
-
-        while(!gamepad1.a){}
-        robot.driveStraight(3,-90,.87);
-        robot.driveStraight(19, 0, .87);
-
-        while(!gamepad1.a){}
-        timesScanned = 0;
         while(!robot.isDetectingRed())
         {
             if (timesScanned > 14)
                 break;
 
+            timesScanned++;
             robot.driveStraight(1,0,.87);
-            sleep(200);
         }
 
-        while(!gamepad1.a){}
-        robot.driveStraight(6,90,.87);
+        robot.driveStraight(5,90,.87);
+        robot.driveStraight(1,-90,.87);
+
+        robot.driveStraight(29 - timesScanned, 0, .87);
+        robot.driveStraight(9, 90,.2);
+        sleep(2000);
+
+        robot.driveStraight(1,-90,.87);
+        robot.driveStraight(15, 0, .87);
+        robot.spin(-7);
+
+        timesScanned = 0;
+        while(!robot.isDetectingRed())
+        {
+            if (timesScanned > 10)
+                break;
+
+            robot.driveStraight(1,0,.87);
+        }
+
+        robot.driveStraight(8,90,.87);
         sleep(100);
 
-        while(!gamepad1.a){}
         if (timesScanned <= 4)
-        {
-            robot.driveStraight(70,-138,.95);
-            robot.spin(180);
-            robot.driveStraight(8, 42, .95);
-        }
+            robot.driveStraight(64, -145, .95);
         else
-        {
-            robot.driveStraight(64, -143, .95);
-            robot.spin(180);
-            robot.driveStraight(8, 35, .95);
-        }
+            robot.driveStraight(70,-138,.95);
 
+        robot.spin(180);
+        robot.driveStraight(9, 47, .95);
     }
 }
